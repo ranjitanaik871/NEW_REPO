4ceb8dcf2e338c1148bb22cf1fff0c2a3fe70912
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-01 21:24:28 +0530

    personal information of students

--- demo.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter a number: "))
-sum = 0
-
-while num > 0:
-    digit = num % 10
-    sum = sum + digit
-    num = num // 10
-print("Sum of digits =", sum)
---
+++ personalinfo.py
@@ -0,0 +1,9 @@
+Name=input("enter the student name:")
+Age=int(input("enter the age:"))
+College=input("enter the college name:")
+Course=input("enter the course name:")
+print("Student name:",Name)
+print("Age:",Age)
+print("college name:",College)
+print("course name:",Course)
+        
