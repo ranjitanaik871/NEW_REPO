a84776f8e04658fae48171e6e3ffb27d4147d2d7
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-03 15:52:29 +0530

    employee bonus eleigibilty

---
+++ task4.py
@@ -0,0 +1,9 @@
+current_stock=int(input("Enter the current stock: "))
+minimum_stock=int(input("Enter the minimum stock level: "))
+minimum_capacity=int(input("Enter the minimum capacity: "))
+if current_stock<minimum_stock:
+    print("Stock is low.")  
+elif current_stock>minimum_capacity:
+    print("overstocked.")
+else:
+    print("optimal.")
---
+++ task5.py
@@ -0,0 +1,6 @@
+years_of_experience=int(input("Enter the years of experience: "))
+performance_rating=int(input("Enter the performance rating : "))
+if years_of_experience>2 and performance_rating>=8:
+    print("Eligible for promotion.")
+else:
+    print("Not eligible for promotion.")    
