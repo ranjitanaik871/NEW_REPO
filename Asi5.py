b8a49953c9866b3f82741f181dffd259bb5089ad
ranjitanaik871 <ranjitanaik871@gmail.com>
2026-07-04 16:27:57 +0530

    Assignment 5 added

---
+++ Asi5.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the number of mobiles: "))
+mobile_name="iphone"
+mobile_price=int(input("Enter the price of the mobile: "))
+
+#calculate the total price of mobiles
+total_price=mobile_price*n
+print("Total price of", n, mobile_name, "is :", total_price)
